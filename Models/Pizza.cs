86ec744424ad51f77957628f249c899e1ac8c6d9
Joseph Felker <jayefelker@gmail.com>
2023-06-05 09:56:39 -0500

    first commit

---
+++ Models/Pizza.cs
@@ -0,0 +1,11 @@
+namespace ContosoPizza.Models;
+
+
+
+public class Pizza2
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+}
+
